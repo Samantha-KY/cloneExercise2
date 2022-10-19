9f82166af42946ef9ea93195b981bbff6c030296
Samiskina <mureraksamantha@gmail.com>
2022-10-19 17:47:34 +0200

    feat: creating stripe landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "primary",
+        secondary: "secondary",
+      },
+      colors: {
+        "graySemi": "#0a2540",
+        "blue": "#635bff",
+        "blue-sky": "#00d4ff",
+        "blue-bright": "#02bcf5",
+        "violet": "#7a73ff",
+      },
+    },
+  },
+  plugins: [],
+};
