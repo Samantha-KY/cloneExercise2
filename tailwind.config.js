9e2690dcbdfdd0d8eb17918e85051b7d77654852
Samiskina <mureraksamantha@gmail.com>
2022-10-20 17:09:05 +0200

    feat: removed gradient.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,18 @@ module.exports = {
         secondary: "secondary",
       },
       colors: {
-        "graySemi": "#0a2540",
-        "blue": "#635bff",
-        "blue-sky": "#00d4ff",
-        "blue-bright": "#02bcf5",
-        "violet": "#7a73ff",
+        "semi-gray": "#0a2540",
+        "stripe-blue": "#635bff",
+        "stripe-sky-blue": "#00d4ff",
+        "stripe-deco-blue": "#02bcf5",
+        "stripe-semi-violet": "#7a73ff",
       },
       screens: {
-        sm: { max: '800px' },
-        md: { min: '1024px' }
-      }
+        sm: { max: "700px" },
+      },
+      padding: {
+        mobile: "2rem",
+      },
     },
   },
   plugins: [],
